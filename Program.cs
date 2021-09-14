8f4709aa5e06c7adc4146c9d1f80c8d2f3be2c2e
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 15:05:37 -0300

    Exercício Feito

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n1, n2, soma;
+            Console.Write("Digite o primeiro número:");
+            n1= int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número:");
+            n2= int.Parse(Console.ReadLine());
+           
+            soma= n1 + n2;
+            Console.Write("/n Soma:");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(soma);
+            Console.ResetColor();
+
+        }
+    }
+}
